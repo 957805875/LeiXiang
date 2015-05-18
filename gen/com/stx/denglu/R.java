ccf304440528a7a63e84e95ebd7383c7bfaae763
xiaohaibin <xhb@xhb-PC>
2015-05-18 17:20:36 +0800

    Lexiang

--- gen/com/stx/denglu/R.java
+++ gen/com/stx/denglu/R.java
@@ -26,6 +26,11 @@ public final class R {
         public static final int Tiny_TextSize=0x7f050007;
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -54,27 +59,29 @@ public final class R {
         public static final int person=0x7f02000e;
         public static final int servlet=0x7f02000f;
         public static final int user=0x7f020010;
-        public static final int welcome6=0x7f020011;
-        public static final int welcome7=0x7f020012;
-        public static final int welcome_image1=0x7f020013;
-        public static final int welcome_image4=0x7f020014;
-        public static final int welcome_image5=0x7f020015;
-        public static final int whats_new_03_01=0x7f020016;
-        public static final int whats_new_bg_01=0x7f020017;
-        public static final int whats_new_bg_04=0x7f020018;
-        public static final int whats_new_bg_06=0x7f020019;
-        public static final int whats_new_start_btn=0x7f02001a;
-        public static final int whats_new_start_btn_normal=0x7f02001b;
-        public static final int whats_new_start_btn_pressed=0x7f02001c;
-        public static final int whatsnew_left=0x7f02001d;
-        public static final int whatsnew_right=0x7f02001e;
-        public static final int xsearch_loading=0x7f02001f;
+        public static final int welcom=0x7f020011;
+        public static final int welcome6=0x7f020012;
+        public static final int welcome7=0x7f020013;
+        public static final int welcome_image1=0x7f020014;
+        public static final int welcome_image4=0x7f020015;
+        public static final int welcome_image5=0x7f020016;
+        public static final int whats_new_03_01=0x7f020017;
+        public static final int whats_new_bg_01=0x7f020018;
+        public static final int whats_new_bg_04=0x7f020019;
+        public static final int whats_new_bg_06=0x7f02001a;
+        public static final int whats_new_start_btn=0x7f02001b;
+        public static final int whats_new_start_btn_normal=0x7f02001c;
+        public static final int whats_new_start_btn_pressed=0x7f02001d;
+        public static final int whatsnew_left=0x7f02001e;
+        public static final int whatsnew_right=0x7f02001f;
+        public static final int xsearch_loading=0x7f020020;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
+        public static final int RelativeLayout1=0x7f090008;
+        public static final int action_settings=0x7f090010;
         public static final int btn_yonghu_zhuce=0x7f090004;
-        public static final int image1=0x7f09000e;
-        public static final int imageView1=0x7f09000d;
+        public static final int image1=0x7f09000f;
+        public static final int imageView1=0x7f090009;
         public static final int image_icon=0x7f090006;
         public static final int linearLayout1=0x7f090000;
         public static final int login_login_btn=0x7f090003;
@@ -82,22 +89,24 @@ public final class R {
         public static final int login_user_edit=0x7f090001;
         public static final int textView1=0x7f090005;
         public static final int textView2=0x7f090007;
-        public static final int title_tv=0x7f09000c;
-        public static final int whats_new_seven_bg_ll=0x7f090008;
-        public static final int whats_new_start_btn=0x7f09000b;
-        public static final int whats_new_title1_tv=0x7f090009;
-        public static final int whats_new_title2_tv=0x7f09000a;
+        public static final int title_tv=0x7f09000e;
+        public static final int whats_new_seven_bg_ll=0x7f09000a;
+        public static final int whats_new_start_btn=0x7f09000d;
+        public static final int whats_new_title1_tv=0x7f09000b;
+        public static final int whats_new_title2_tv=0x7f09000c;
     }
     public static final class layout {
-        public static final int activity_sign_in=0x7f030000;
-        public static final int layout_line=0x7f030001;
-        public static final int whats_news_gallery_four=0x7f030002;
-        public static final int whats_news_gallery_one=0x7f030003;
-        public static final int whats_news_gallery_three=0x7f030004;
-        public static final int whats_news_gallery_two=0x7f030005;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_start=0x7f030001;
+        public static final int layout_line=0x7f030002;
+        public static final int whats_news_gallery_four=0x7f030003;
+        public static final int whats_news_gallery_one=0x7f030004;
+        public static final int whats_news_gallery_three=0x7f030005;
+        public static final int whats_news_gallery_two=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f080000;
+        public static final int sign_in=0x7f080001;
     }
     public static final class string {
         public static final int Forgetpassword=0x7f060010;
@@ -106,6 +115,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int title_activity_sign_in=0x7f060011;
         public static final int whats_new_chongqing=0x7f060008;
         public static final int whats_new_daybeforeyesterday=0x7f06000b;
         public static final int whats_new_march=0x7f06000c;
